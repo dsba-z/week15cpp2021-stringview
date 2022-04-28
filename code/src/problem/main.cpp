1cecb06e05af62aa7fd5ea704672ed59aa02392f
Georgii Zhulikov <l8doku@gmail.com>
2022-04-28 12:54:27 +0300

    Workshop 30

--- code/src/problem/main.cpp
+++ code/src/problem/main.cpp
@@ -2,10 +2,24 @@
 #include <fstream>
 #include <string_view>
 #include <string>
+#include <cstring>
 #include <vector>
 #include <chrono>
 #include <ctime>
 #include <iomanip>
+
+// line is shorter than 50 symbols
+// the first characters is 'A'
+bool isLineGood(const std::string& line)
+{
+    if (line[0] == 'A' && line.size() < 50)
+    {
+        return true;
+    }
+    return false;
+}
+
+
 
 
 void collectLines(const std::string& text, std::vector<std::string>& lineBag)
@@ -19,7 +33,7 @@ void collectLines(const std::string& text, std::vector<std::string>& lineBag)
         {
             lineBag.push_back(text.substr(lineStart, lineLength));
             lineLength = 0;
-            lineStart = i;
+            lineStart = i+1;
         }
         else
         {
@@ -40,7 +54,7 @@ void collectLinesSv(std::string_view text, std::vector<std::string_view>& lineBa
         {
             lineBag.push_back(text.substr(lineStart, lineLength));
             lineLength = 0;
-            lineStart = i;
+            lineStart = i+1;
         }
         else
         {
@@ -91,6 +105,42 @@ void runStdStringView(const char* buffer)
               << " ms\n";
 }
 
+
+
+void checkGoodLines(const char* buffer)
+{
+    std::vector<std::string> wordSet;
+    collectLines(buffer, wordSet);
+    
+    int counter = 0;
+
+    std::clock_t c_start2 = std::clock();
+    auto t_start2 = std::chrono::high_resolution_clock::now();
+    
+    // checking all lines
+    for(const std::string& line: wordSet)
+    {
+        if (isLineGood(line))
+        {
+            std::cout << line << std::endl;
+            counter++;
+        }
+    }
+    
+    std::clock_t c_end2 = std::clock();
+    auto t_end2 = std::chrono::high_resolution_clock::now();
+ 
+    std::cout << std::fixed << std::setprecision(2) << "CPU time used: "
+              << 1000.0 * (c_end2 - c_start2) / CLOCKS_PER_SEC << " ms\n"
+              << "Wall clock time passed: "
+              << std::chrono::duration<double, std::milli>(t_end2-t_start2).count()
+              << " ms\n";
+    
+    std::cout << counter << std::endl;
+}
+
+
+
 void task0()
 {
     // the size of the book is 160000+ symbols
@@ -103,35 +153,39 @@ void task0()
     runStdString(buffer);
     
     runStdStringView(buffer);
-
-}
-
-
-// line is shorter than 50 symbols
-// the first characters is 'A'
-bool isLineGood(/* input */)
-{
-    return true;
-}
-
-
-void task1()
-{
-    if (isLineGood(/* "A good line" */))
-    {
-        std::cout << "first line is good";
-    }
     
-    std::string secondLine("Not a good line");
-    if (!isLineGood(/* secondLine */))
-    {
-        std::cout << "second line is not good";
-    }
-    
+    checkGoodLines(buffer);
+
 }
+
+
+
+//void task1()
+//{
+//    if (isLineGood())
+//    {
+//        std::cout << "first line is good";
+//    }
+    
+//    std::string secondLine("Not a good line");
+//    if (!isLineGood())
+//    {
+//        std::cout << "second line is not good";
+//    }
+    
+//}
+
+
 
 int main()
 {
+    
+    std::string example("string");
+    
+    const char* pointer = example.c_str();
+    
+    
+    int a = strlen("asdasdasd");
     task0();
 //    task1();
 
